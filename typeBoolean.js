d328f69cc90ffa372c67d894f4da3b00c5c1dfba
paSelva <paulo.goncalves0406@gmail.com>
2023-03-07 17:10:56 -0300

    atualizacao boleanos

--- typeBoolean.js
+++ typeBoolean.js
@@ -7,3 +7,13 @@ const texto1 = 'alura';
 const texto2 = 'a';
 
 console.log(texto1 === texto2);
+
+//truthy ou falsy
+
+//0 = false
+//1 = true
+console.log(0 == false);
+console.log('' == false);
+console.log(1 == true);
+//null = dado que representa vazio
+//undefined = variavel nao definida
