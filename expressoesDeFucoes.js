15aa38d9c7e7c11bc40c3847bdd50533329913bb
paSelva <paulo.goncalves0406@gmail.com>
2023-03-08 20:46:48 -0300

    atualização final

--- expressoesDeFucoes.js
+++ expressoesDeFucoes.js
@@ -9,3 +9,14 @@ console.log(soma(1,1))
 
 const apresentarArrow = nome => `Meu nome é ${nome}`;
 const soma1 = (num1,num2) => num1+num2;
+
+//Arrow com mais de uma linha
+
+const somanumeros = (num1,num2) => {
+    if(num1>10|| num2>10){
+        return 'somente numeros de 1 a 9'
+    }
+    else{
+        return num1+num2;
+    }
+}
