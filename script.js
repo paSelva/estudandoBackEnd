f28fe482422b6a6687e114551dd6d732f5bd1564
paSelva <paulo.goncalves0406@gmail.com>
2023-03-07 21:35:12 -0300

    add novo arquivo e conteudo

--- script.js
+++ script.js
@@ -3,7 +3,15 @@
 
 
 
-//Erros
+//Erros identificando
 
 const numero = 1;
-console.log(numero
+console.log(numero)
+
+//console.log = registro 
+//tratamento de erro
+console.error('deu erro')
+//console.error() para exibir mensagens de erro;
+//console.table() para visualizar de forma mais organizada informações tabulares;
+//console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+//console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
